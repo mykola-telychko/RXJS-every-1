c5d2fed9b692027b4c94022cb916c5b14e75f98a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-12 13:01:34 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { every } from 'rxjs/operators';
 import { of, interval } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/conditional/every
+
 //emit 5 values
-const source = of(1, 2, 3, 4, 5);
-const src = of(22, 2, 66, 4, 6);
+const source = of(1, 2, 3, 4, 5); // false (because not all is even)
+const src = of(22, 2, 66, 4, 6); // every value even
 
 const example_1 = source.pipe(
   //is every value even?
@@ -16,5 +18,5 @@ const example_2 = src.pipe(
 );
 
 //output: false
-// const subscribe = example.subscribe((val) => console.log(val));
+// const subscribe = example_1.subscribe((val) => console.log(val));
 const subscribe = example_2.subscribe((val) => console.log(val));
